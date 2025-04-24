a9b75f05e1d3574fab50e39e31956f7bcf1b5401
Baho Kemana Joselyto Charite <88556861+Josy-coder@users.noreply.github.com>
2025-04-24 14:35:58 +0200

    [chore] update nextconfig for cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig: import('next').NextConfig = {
   reactStrictMode: true,
-};
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+}
 
-export default nextConfig;
+module.exports = nextConfig
